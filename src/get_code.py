9ff671c622f8034dbcf6867bce5a4ddf16229cbd
Sapphire <33162278+sapphire-bt@users.noreply.github.com>
2025-03-29 13:07:07 +0000

    Add reg value for app orders

--- src/get_code.py
+++ src/get_code.py
@@ -1,11 +1,17 @@
 import argparse
 
 from datetime import datetime
+from enum import IntEnum
+
 
 CHARS = "CM7WD6N4RHF9ZL3XKQGVPBTJY"
 BASE = len(CHARS)
 EPOCH_BEGIN = datetime(2016, 2, 1)
-REG_DELIVERY = 61
+
+
+class Reg(IntEnum):
+    DELIVERY = 61
+    APP = 300
 
 
 def encode(num):
@@ -59,9 +65,11 @@ def get_check_digit(code):
 
 def generate_code(store_id, order_id, purchased, reg):
     zero = encode(0)  # Used for padding
+    if reg == Reg.DELIVERY:
 
-    if reg == REG_DELIVERY:
         reg = 0
+    elif reg == Reg.APP:
+        reg = 1
 
     enc_store_id = encode(store_id).rjust(3, zero)
     enc_order_id = encode((order_id % 100) + (reg * 100)).rjust(3, zero)
